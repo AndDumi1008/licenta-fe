8fc0b333792302b718b8486aa81038e9bdf76088
Andrei Dumitrescu <andrei.dumitrescu100801@gmail.com>
2022-09-28 00:05:56 +0300

    add component files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,11 @@ import {CodemirrorModule} from "@ctrl/ngx-codemirror";
 import {CommonModule} from "@angular/common";
 import { MenuListComponent } from './menu-list/menu-list.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
+import {FaIconLibrary, FontAwesomeModule} from "@fortawesome/angular-fontawesome";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+
 
 @NgModule({
   declarations: [
@@ -26,9 +31,16 @@ import { TopBarComponent } from './top-bar/top-bar.component';
     FormsModule,
     MatCardModule,
     CommonModule,
-    CodemirrorModule
+    CodemirrorModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(library: FaIconLibrary) {
+    // Add an icon to the library for convenient access in other components
+    // library.addIcons(faCircleInfo, faCoffee);
+    library.addIconPacks(fas, far, fab);
+  }
+}
