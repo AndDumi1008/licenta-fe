30e99f0dc920655814b7c61e3e1f7fe24df109bb
Andrei Dumitrescu <andrei.dumitrescu100801@gmail.com>
2023-01-30 15:41:14 +0200

    Solved bug of side nav

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,9 @@ module.exports = {
         'white80': '#FFFFFFCC',
         'grey20': '#00000033',
         'grey30': '#0000004D',
+      },
+      spacing: {
+        15:'3.75rem',
       }
     },
   },
