807a6c09cf11393f81623b8bea6e6c40656af7da
Andrei Dumitrescu <andrei.dumitrescu100801@gmail.com>
2023-03-16 12:02:37 +0200

    added getAnswer() to return existing answer

--- src/app/_services/course.service.ts
+++ src/app/_services/course.service.ts
@@ -5,6 +5,7 @@ import {environment} from "../../environments/environment";
 import {Observable} from "rxjs";
 import {ICourseDetails} from "../_interfaces/ICourseDetails";
 import {GlobalVariableService} from "./global-variable.service";
+import {IAnswer} from "../_interfaces/IAnswer";
 
 @Injectable({
   providedIn: 'root'
@@ -24,5 +25,9 @@ export class CourseService {
 
   getCourse(id?: string): Observable<ICourseDetails> {
     return this.http.get<ICourseDetails>(`${this.apiUrl}/course/${id}`, {headers: this.header.getHeaderOptions()})
+  }
+
+  getAnswer(userId?: string, labId?: string) {
+    return this.http.get<IAnswer>(`${this.apiUrl}/lab=${labId}/${userId}`, {headers: this.header.getHeaderOptions()})
   }
 }
