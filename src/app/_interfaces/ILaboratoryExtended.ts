db31f8e225544e5c6337284e91f2c673eef17023
Andrei Dumitrescu <andrei.dumitrescu100801@gmail.com>
2023-06-22 22:58:06 +0300

    small change

---
+++ src/app/_interfaces/ILaboratoryExtended.ts
@@ -0,0 +1,11 @@
+export interface ILaboratoryExtended {
+  id: string | null,
+  course: string,
+  priority: number,
+  title: string,
+  content: string,
+  exercise: string,
+  codeInput: string,
+  codeOutput: string,
+  codeLanguage: string
+}
